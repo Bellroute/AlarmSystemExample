3b2e925fcd6553df0cbad360b4e1ebd949ac6269
Bellroute <kjkun7631@gmail.com>
2024-03-28 17:50:44 +0900

    init project

---
+++ src/main/java/com/example/alarm_system_example/AlarmSystemExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.alarm_system_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmSystemExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlarmSystemExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alarm_system_example/AlarmSystemExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alarm_system_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmSystemExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
